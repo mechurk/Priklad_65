34e9974c2b9826b70c5075580d495942cb357cbe
mechurk <londynska56>
2019-02-07 21:28:15 +0100

    skript

--- pr_65.py
+++ pr_65.py
@@ -1,2 +1,20 @@
 # coding=utf-8
-import arcpy, sys, os, ascii
+import random
+
+def listofrandomnum (number, a, b):
+    list =[]
+    for i in range(number):
+        randnum=random.randint(a,b)
+        list.append(randnum)
+    return list
+
+list1= listofrandomnum(100,10,110)
+list2= listofrandomnum(100,10,110)
+print (list1)
+print (list2)
+
+result=[]
+for i in list1:
+    if i in list2:
+        result.append(i)
+print (list(set(result)))
