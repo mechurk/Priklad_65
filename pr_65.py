18a432cb8c4f79d1d74304dcdda9a803da37dbbf
mechurk <londynska56>
2019-02-09 12:01:39 +0100

    uprava

--- pr_65.py
+++ pr_65.py
@@ -1,7 +1,21 @@
 # coding=utf-8
-import random
 
+"""
+Závěrečný úkol pro předmět: Úvod do Programování.
+Zadání úkolu: Dány dvě posloupnisti čísel. Nalezení jejich průniku (společné prvky)
+Vypracovala: Kristýna Měchurová, rok: 2019
+"""
+
+import random
+#definování funkcí
 def listofrandomnum (number, a, b):
+    """ Vytvoří list náhodných celých čísel.
+        Vstup:  number - počet čísel v řetězci
+                a, b  - interval náhodných čísel
+                (a: minimální hodnota intervalu
+                 b: maximální hodnota intervalu)
+        Výstup: list náhodných čísel.
+                """
     list =[]
     for i in range(number):
         randnum=random.randint(a,b)
@@ -9,14 +23,32 @@ def listofrandomnum (number, a, b):
     return list
 
 def intersection (list1, list2):
+    """ Průnik dvou listů obsahujících celá čísla.
+        Vstup:  list1,list2 - list celých čísel
+        Výstup: průnik vstupů ve formátu list
+    """
+    list1=listofint(list1)
+    list2 = listofint(list2)
     result=[]
     for i in list1:
         if i in list2:
             result.append(i)
-    return (list(set(result)))
+    return (list(set(result))) #odstranění duplicit z průniku
 
-list1= listofrandomnum(100,10,110)
-list2= listofrandomnum(100,10,110)
+def listofint (listofnum):
+    """Ošetření list obsahuje pouze celá čísla
+      Vstup: list
+      Výstup: list obsahující pouze celá čísla"""
+
+    if all(isinstance(i, int) for i in listofnum)==True:
+        return listofnum
+    else:
+        print ("List neobsahuje pouze celá čísla")
+        quit (1)
+
+#spouštění funkcí
+list1= listofrandomnum(10,5,20)
+list2= listofrandomnum(10,5,20)
 print (list1)
 print (list2)
 print (intersection(list1,list2))
